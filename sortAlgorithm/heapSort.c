a942ef66c94d9ffdc55a572a172d097403faf4a8
wait0804 <15802953651@163.com>
2017-04-14 07:52:51 +0800

    wrote ten sortAlgorithm

---
+++ sortAlgorithm/heapSort.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+ 
+void heapSort(int *a, int count);
+void siftDown(int *a, int count, int start);
+void swap(int *a, int *b);
+int removeHeap(int *a, int n);
+ 
+int removeHeap(int *a, int n){
+    int key = a[0];
+ 
+    a[0] = a[n];
+    siftDown(a, n, 0);
+ 
+    return key;
+}
+ 
+void swap(int *a, int *b){
+    int tmp;
+ 
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+ 
+void siftDown(int *a, int count, int start){
+    int i = start;
+    int j = 2*i+1;
+ 
+    while(j < count){  //说明有左子树
+        if(j < count-1 && a[j] > a[j+1]){ //表示存在右孩子的情况下；
+            j++;   //j：表示指向左右子树中最小的一个
+        }
+        if(a[i] <= a[j]){
+            break;  //不用调整，父节点的值是最小的；
+        }else{
+            swap(&a[i], &a[j]);
+            //交换
+            i = j; //一直往树下交换，一直调整到叶子结点
+            j = 2*i+1;
+        }
+    }
+}
+ 
+void heapSort(int *a, int count){
+    int curPos = count/2-1; //最后一个非叶子结点
+    int i;
+    int key;
+ 
+    while(curPos >= 0){
+        siftDown(a, count, curPos);
+        curPos--;
+    }
+/*  输出构建好的堆结构
+    for(i = 0; i < count; i++){
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+*/
+ 
+    for(i = 0; i < count; i++){
+        key = removeHeap(a, count-i-1);  //传参：第二个参数是下标
+        printf("%d ", key);
+    }
+    printf("\n");
+}
+ 
+void main(void){
+    int a[] = {3, 5, 7, 1, 4, 2, 9, 10};
+    int count = sizeof(a)/sizeof(int);
+ 
+    heapSort(a, count);
+}
---
+++ sortAlgorithm/shellSort.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+ 
+void insertSort(int *a, int count, int step);
+void showArray(int *a, int count);
+void shellSort(int *a, int count);
+ 
+void shellSort(int *a, int count){
+    int step;
+ 
+    for(step = count/2; step > 0; step /= 2){
+        insertSort(a, count, step);
+    }
+}
+ 
+void showArray(int *a, int count){
+    int i;
+     
+    for(i = 0; i < count; i++){
+        printf("%d ", a[i]);
+    }
+    printf("\n"); 
+}
+//以下就是插入排序了，思想完全一样，只不过步长为step而已！！！
+void insertSort(int *a, int count, int step){
+    int i;
+    int j;
+    int n;
+    int tmp;
+ 
+    for(i = step; i < count; i+=step){
+        tmp = a[i];
+        for(j = 0; a[i]>a[j] && j<i; j+=step){
+            ;
+        }
+        if(i != j){
+            for(n = i; n > j; n-=step){
+                a[n] = a[n-step];
+            }
+            a[j] = tmp;
+        }
+    }
+}
+ 
+void main(void){
+    int a[] = {2, 7, 1, 11, 0, 9, 8, 10};
+    int count = sizeof(a)/sizeof(int);
+ 
+    printf("排序前输出如下: ");
+    showArray(a, count);
+    shellSort(a, count);
+    printf("排序后输出如下: ");
+    showArray(a, count);
+ 
+}
